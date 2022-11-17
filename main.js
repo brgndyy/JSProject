cb4da19b9444be02bf87b0ff72abf564edbd3ff7
brgndy <brgndy96@gmail.com>
2022-11-17 12:01:03 +0900

    last

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const filter = document.getElementById("filter");
 const postContainer = document.getElementById("postContainer");
-const loadingIcon = document.getElementById("loadingIcon");
+const loadingIcon = document.getElementById("loadingContainer");
 
 let limit = 5;
 let page = 1;
@@ -17,28 +17,53 @@ const getRandomPost = async () => {
 };
 
 const addToDOMData = (data) => {
-  postContainer.innerHTML = `${data
-    .map(
-      (item) => `<div class="posts">
-    <div class="number">${item.id}</div>
+  data.forEach((item) => {
+    const post = document.createElement("div");
+    post.classList.add("posts");
+
+    post.innerHTML = `<div class="number">${item.id}</div>
     <div class="post-info">
         <div class="post-title">${item.title}
         </div>
         <div class="post-body">${item.body}
         </div>
-    </div>
-</div>`
-    )
-    .join("")}`;
+    </div>`;
+
+    postContainer.appendChild(post);
+  });
 };
+
 
 getRandomPost();
 
+document.addEventListener("scroll", (e) => {
+  const { clientHeight, scrollTop, scrollHeight } = e.target.scrollingElement;
 
-window.addEventListener("scroll", () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight-5) {
-    
-    getRandomPost();
-    page++;
+  if (clientHeight + scrollTop >= scrollHeight) {
+    setTimeout(() => {
+      page++;
+      getRandomPost();
+    }, 500);
   }
 });
+
+const filterPost = (e) => {
+    const words = e.target.value;
+    const posts = document.querySelectorAll(".posts");
+
+    posts.forEach(post => {
+        const title = post.querySelector('.post-title').innerText;
+        const body = post.querySelector('.post-body').innerText;
+
+       if(title.indexOf(words) > -1 || body.indexOf(words) > -1){
+        post.style.display = 'flex';
+       } else{
+        post.style.display = 'none';
+       }
+    })
+    
+}
+
+
+filter.addEventListener('input', filterPost);
+
